3103a73e5d8f2b3002b236137e6d5a61aae1668d
高俊东 <shandaowufu@qq.com>
2021-03-22 14:48:30 +0800

    ✨ feat(all): 添加git

---
+++ config.js
@@ -0,0 +1,41 @@
+/**
+ * Created by WebStorm.
+ * User: 高俊东
+ * Date: 3/22/21
+ * Time: 12:44 PM
+ */
+
+module.exports = [
+    {
+      value: '✨ feat',
+      name : '✨ feat: 新功能'
+    },
+    {
+      value: '🐞 fix',
+      name : '🐞 fix: bug修复'
+    },
+    {
+      value: '🛠  refactor',
+      name : '🛠  refactor: 代码重构'
+    },
+    {
+      value: '📚 docs',
+      name : '📚 docs: 文档修改'
+    },
+    {
+      value: '🏁 test',
+      name : '🏁 test: 测试'
+    },
+    {
+      value: '🗯  chore',
+      name : '🗯  chore: 未修改重要文件，比如: 打包配置等'
+    },
+    {
+      value: '🌈 style',
+      name : '🌈 style: 样式文件'
+    },
+    {
+      value: '⏪ revert',
+      name : '⏪ revert: 版本回退'
+    }
+  ]
